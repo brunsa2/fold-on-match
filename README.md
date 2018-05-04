90ec7efd61ab26bc5a9a3f0de7b105683ab94806
Jeff Stubler <brunsa2@gmail.com>
2018-05-03 22:02:21 -0500

    Remove unneeded files and prepare for first publish

---
+++ README.md
@@ -0,0 +1,5 @@
+Fold on Match
+=============
+
+Fold or unfold code at every match of the currently selected text or word that
+the cursor is in.
--- spec/fold-on-match-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import FoldOnMatchView from '../lib/fold-on-match-view';
-
-describe('FoldOnMatchView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
